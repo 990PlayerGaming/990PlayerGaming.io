9efe8fe2189194888f4688734d48e787ac2efa83
990PlayerGaming <lucadanieldiana@gmail.com>
2024-02-29 21:52:55 +0100

    a

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function CambiaTema(){
+    if(document.getElementById("bg").style.backgroundColor != "white"){
+        document.getElementById("bg").style.backgroundColor = "white"
+        document.getElementsById("majoscritta").style.color = "black"
+        
+        
+    }else{
+        document.getElementById("bg").style.backgroundColor = "#1E2935"
+    }
+}
+
